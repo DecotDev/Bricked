9992880e1e39b015e9befd49ffd9b2cf77e8a228
DecotDev <decot013@gmail.com>
2026-04-18 18:37:30 +0200

    Implement brick placement and add player & assets
    Add player scene and new brick assets, and implement brick placement logic. brick_28.gd: replace floor check with a place_block() routine that uses RayCast2D, stops falling, switches collision layers (falling -> placed), disables Area2D monitoring and stops physics processing. brick_28.tscn: adjust collision layers/masks, add RayCast2D, hide/disable Area2D and remove old area signal. main_28.tscn: add a new Brick32 instance. Add brick4 and brick8 PNGs with import settings and generated .ctex/.md5 import artifacts. player/player.tscn: add icon texture resource, change collision layer/mask and add a Sprite2D node. Also update several Godot editor metadata/cache files (open scenes, selected paths, filesystem cache/update, quick open and script editor caches) to reflect the new/modified resources.

--- bricks/brick28/brick_28.gd
+++ bricks/brick28/brick_28.gd
@@ -9,7 +9,15 @@ func _ready() -> void:
 	locked_x = position.x
 
 func _physics_process(delta: float) -> void:
-	check_floor4()
+	#check_floor4()
+	#move_and_slide()
+
+	if not $RayCast2D.is_colliding():
+		velocity = Vector2(0, fall_speed)
+	else:
+		velocity = Vector2.ZERO
+		place_block()
+
 	move_and_slide()
 	
 func check_floor4():
@@ -21,11 +29,23 @@ func check_floor4():
 		set_physics_process(false)
 	else:
 		velocity = Vector2(0,fall_speed)
+		
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print(str(name) + (" ha detectado a: ") + str(body.name))
-	#if body.is_class("Block") or body.name == "StaticBody2D" :
-	if body.name == "Player":
-		pass
-	else:
-		touched_a_block = true
+func place_block():
+	print("PLACED")
+	velocity = Vector2.ZERO
+	
+	# 🔥 CAMBIO DE CAPA
+	set_collision_layer_value(3, false) # deja de ser falling
+	set_collision_layer_value(2, true)  # ahora es placed
+	
+	$Area2D.monitoring = false
+	
+	set_physics_process(false)
+
+#func _on_area_2d_body_entered(body: Node2D) -> void:
+	#print(str(name) + (" ha detectado a: ") + str(body.name))
+	#if body.name == "Player":
+		#pass
+	#else:
+		#touched_a_block = true
